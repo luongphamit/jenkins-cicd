f3b0abf7e2d570c09a42a7325221c479a4b0c3b6
luongphamit <luongphamit@gmail.com>
2024-05-09 07:34:27 +0700

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+sudo docker run --name jenkins-blueocean --restart=on-failure --detach   --network jenkins --env DOCKER_HOST=tcp://docker:2376   --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=0   --publish 8080:8080 --publish 50000:50000   --volume jenkins-data:/var/jenkins_home   --volume jenkins-docker-certs:/certs/client:ro   myjenkins-blueocean:lts-jdk17
+ 1826  sudo docker run -d --restart=always -p 127.0.0.1:2376:2375 --network jenkins -v /var/run/docker.sock:/var/run/docker.sock alpine/socat tcp-listen:2375,fork,reuseaddr unix-connect:/var/run/docker.sock
