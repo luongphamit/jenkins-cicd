452f95a38655f3f3e216eedd1b79af2ea40bee26
luongphamit <luongphamit@gmail.com>
2024-07-06 13:20:09 +0700

    ocean build

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,32 @@
-sudo docker run --name jenkins-blueocean --restart=on-failure --detach   --network jenkins --env DOCKER_HOST=tcp://docker:2376   --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=0   --publish 8080:8080 --publish 50000:50000   --volume jenkins-data:/var/jenkins_home   --volume jenkins-docker-certs:/certs/client:ro   myjenkins-blueocean:lts-jdk17
- 1826  sudo docker run -d --restart=always -p 127.0.0.1:2376:2375 --network jenkins -v /var/run/docker.sock:/var/run/docker.sock alpine/socat tcp-listen:2375,fork,reuseaddr unix-connect:/var/run/docker.sock
+sudo docker network create jenkins
+
+sudo docker rm -f jenkins-docker jenkins-blueocean
+
+sudo docker run \
+    --name jenkins-docker \
+    --rm \
+    --detach \
+    --privileged \
+    --network jenkins \
+    --network-alias docker \
+    --env DOCKER_TLS_CERTDIR=/certs \
+    --volume jenkins-docker-certs:/certs/client \
+    --volume jenkins-data:/var/jenkins_home \
+    --publish 2376:2376 \
+    docker:dind \
+    --storage-driver overlay2
+
+
+sudo docker run \
+    --name jenkins-blueocean \
+    --restart=on-failure \
+    --detach \
+    --network jenkins \
+    --env DOCKER_HOST=tcp://docker:2376 \
+    --env DOCKER_CERT_PATH=/certs/client \
+    --env DOCKER_TLS_VERIFY=1 \
+    --publish 8080:8080 \
+    --publish 50000:50000 \
+    --volume jenkins-data:/var/jenkins_home \
+    --volume jenkins-docker-certs:/certs/client:ro \
+    myjenkins-blueocean:2.452.2-1
