38c321d47960a1d6ec8b2062e94c1536ea5e221a
luongphamit <luongphamit@gmail.com>
2024-07-06 14:06:22 +0700

    .

--- deploy.sh
+++ deploy.sh
@@ -25,6 +25,7 @@ sudo docker run \
     --env DOCKER_HOST=tcp://docker:2376 \
     --env DOCKER_CERT_PATH=/certs/client \
     --env DOCKER_TLS_VERIFY=1 \
+    --env JENKINS_UC_DOWNLOAD=http://ftp-nyc.osuosl.org/pub/jenkins/ \
     --publish 8080:8080 \
     --publish 50000:50000 \
     --volume jenkins-data:/var/jenkins_home \
